b572c091998449aac98dfe29b627b45b87d8fa6a
RealView45 <kjink015@gmail.com>
2023-02-05 22:38:34 +0900

    NavigationView중첩하지않기 (중복없애기)

--- WavveApp/LiveView.swift
+++ WavveApp/LiveView.swift
@@ -12,22 +12,22 @@ struct LiveView: View {
     @State var showingActionSheet = false
     var body: some View {
         //id 프로퍼티 추가로 따로 고유아이디넣어줄 필요없어짐livestream.products 뒤 , id: \.name 없앨수있음
-        NavigationView{
-            List{
-                Button(action: {print("liveitem")}){
-                    Text("채널-").bold()+Text("전체장르")
-                }.accentColor(.gray)
-                ForEach(livestream.products){ product in
-                    NavigationLink(destination: LiveStreamView(product: product)){//이동하는 공간
-                        ProductRow(product: product)//누르는 공간
-                    }
-                    
+        
+        List{
+            Button(action: {print("liveitem")}){
+                Text("채널-").bold()+Text("전체장르")
+            }.accentColor(.gray)
+            ForEach(livestream.products){ product in
+                NavigationLink(destination: LiveStreamView(product: product)){//이동하는 공간
+                    ProductRow(product: product)//누르는 공간
                 }
-                .navigationBarTitle("")
                 
-            }.listStyle(PlainListStyle())
+            }
+            .navigationBarTitle("")
             
-        }
+        }.listStyle(PlainListStyle())
+        
+        
     }
     
     struct LiveView_Previews: PreviewProvider {
